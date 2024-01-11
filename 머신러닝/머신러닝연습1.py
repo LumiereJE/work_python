a312b133851838b659b40dea7f040c84acd9853c
lumiereJE <lumiere_remplir@naver.com>
2024-01-11 17:56:42 +0900

    01/11

---
+++ 머신러닝/머신러닝연습1.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plit # 시각화 라이브러리(예제용으로 씀. 우리는 리액트로 할것임)
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier # 인접회귀
+
+# 도미 데이터
+bream_length = [25.4, 26.3, 26.5, 29.0, 29.0, 29.7, 29.7, 30.0, 30.0, 30.7, 31.0, 31.0,
+                31.5, 32.0, 32.0, 32.0, 33.0, 33.0, 33.5, 33.5, 34.0, 34.0, 34.5, 35.0,
+                35.0, 35.0, 35.0, 36.0, 36.0, 37.0, 38.5, 38.5, 39.5, 41.0, 41.0]
+bream_weight = [242.0, 290.0, 340.0, 363.0, 430.0, 450.0, 500.0, 390.0, 450.0, 500.0, 475.0, 500.0,
+                500.0, 340.0, 600.0, 600.0, 700.0, 700.0, 610.0, 650.0, 575.0, 685.0, 620.0, 680.0,
+                700.0, 725.0, 720.0, 714.0, 850.0, 1000.0, 920.0, 955.0, 925.0, 975.0, 950.0]
+
+
+# 빙어 데이터
+smelt_length = [9.8, 10.5, 10.6, 11.0, 11.2, 11.3, 11.8, 11.8, 12.0, 12.2, 12.4, 13.0, 14.3, 15.0]
+smelt_weight = [6.7, 7.5, 7.0, 9.7, 9.8, 8.7, 10.0, 9.9, 9.8, 12.2, 13.4, 12.2, 19.7, 19.9]
+
+# 기본 도트 그래프(분포도?)
+# plt.scatter(bream_length, bream_weight) # 도미
+# plt.scatter(smelt_length, smelt_weight) # 빙어
+# plt.xlabel('length')
+# plt.ylabel('weight')
+# plt.show()
+
+
+# 가장 간단한 머신러닝 프로그램
+# K-최근접 이웃 알고리즘 : 입력받은 데이터의 위치가 어디에 가까운지를 판단해서 결정함
+length = bream_length + smelt_length
+weight = bream_weight + smelt_weight
+fish_data = [[l, w] for l, w in zip(length, weight)]
+print(fish_data)
+
+# 빙어와 도미 구분값 만들기
+fish_target = [1] * 35 + [0] * 14 # 1이면 도미, 0이면 빙어
+print(fish_target)
+
+kn = KNeighborsClassifier()
+kn.fit(fish_data, fish_target) # fit()메서드는 주어진 데이터로 알고리즘을 훈련
+print(kn.score(fish_data, fish_target)) # 실행결과가 1이면 모든 fish_data의 답을 맞췄다는 의미임
+
+# 임의의 값을 넣었을 때, 예측 하기 (도미인지(1), 빙어인지(0) 예측하기)
+print(kn.predict([[30, 600]])) # 예측이 필요한 값 넣기
+
+
+
+
+
